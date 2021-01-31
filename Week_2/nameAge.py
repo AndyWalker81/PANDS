12c75fb58026dc22a1010106485238ce57967f9c
Andy Walker <g00398788@gmit.ie>
2021-01-31 11:31:15 +0000

    Week 02

---
+++ Week_2/nameAge.py
@@ -0,0 +1,10 @@
+# nameAge.py
+# A program that reads in a name and an age and outputs a statement
+# Author: Andy Walker
+
+# The follow lines are the inputs
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+
+# The follow line produces the output
+print("Hello {}, \tyour age is {}.".format(name, age))
---
+++ Week_2/subtraction.py
@@ -0,0 +1,13 @@
+# subtraction.py
+# A program which inputs two numbers and subtracts the second from the first
+# Author: Andy Walker
+
+# these lines get the input
+firstNumber = int(input("Please enter the first number: "))
+print ("The first number is {}".format(firstNumber))
+secondNumber = int(input("Please enter the second number: "))
+print ("The second number is {}".format(secondNumber))
+
+# this line provides the answer
+print ("The answer is: {}".format(firstNumber-secondNumber))
+
