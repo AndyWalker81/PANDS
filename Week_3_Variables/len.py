1e518ca2d897738ecfb19ba5f3fb7365f1c54eb4
Andy Walker <g00398788@gmit.ie>
2021-02-08 13:53:03 +0000

    Week 3

---
+++ Week_3_Variables/len.py
@@ -0,0 +1,6 @@
+# len.py
+# A program that reads in a string and outputs its length
+# Author: Andy Walker 
+
+x = str(input("Please enter a string: "))
+print ("The length of {} is: {}".format(x,(len(x))))
